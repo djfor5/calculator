cecc4418e1cd1af881e198725ea3b3c48a1a191a
djfor5 <117873331+djfor5@users.noreply.github.com>
2023-04-06 14:12:55 +1000

    Add basic arithmetic functions
    Add functions for add, subtract, multiply, and divide to script.js.

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+function add(num1, num2) {
+  return num1 + num2
+}
+
+console.log(`%cadd function`, "font-weight: bold")
+console.log(`add(3,2): ${add(3,2)}`)
+console.log('\n')
+
+
+function subtract(num1, num2) {
+  return num1 - num2
+}
+
+console.log(`%csubtract function`, "font-weight: bold")
+console.log(`subtract(7,3): ${subtract(7,3)}`)
+console.log('\n')
+
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+console.log(`%cmultiply function`, "font-weight: bold")
+console.log(`multiply(2,5): ${multiply(2,5)}`)
+console.log('\n')
+
+
+function divide(num1, num2) {
+  return num1 / num2
+}
+
+console.log(`%cdivide function`, "font-weight: bold")
+console.log(`divide(12,3): ${divide(12,3)}`)
+console.log('\n')
